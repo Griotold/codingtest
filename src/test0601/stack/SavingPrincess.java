39030e6bc33f1357f45c399d2fdf925990b36433
Griotold <gotjd9773@naver.com>
2025-05-29 16:29:28 +0900

    공주 구하기 - 큐

---
+++ src/test0601/stack/SavingPrincess.java
@@ -0,0 +1,32 @@
+package test0601.stack;
+
+import java.util.*;
+
+public class SavingPrincess {
+    // 8 3
+    // 7
+    static int solution(int n, int target) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        for(int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+        int count = 1;
+        while (queue.size() > 1) {
+            if (count == target) {
+                queue.poll();
+                count = 1; // ★ 이 부분이 중요!
+            } else {
+                queue.add(queue.removeFirst());
+                count++;
+            }
+        }
+        return queue.removeFirst();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = sc.nextInt();
+        System.out.println(solution(n, num));
+    }
+}
